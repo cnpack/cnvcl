32a4eb1a3ec67ba563feb0665f3d11ce0e90b877
liuxiao <liuxiao@cnpack.org>
2014-08-14 22:52:23 +0800

    * [Consts] Update Version Number.

--- Source/Common/CnConsts.pas
+++ Source/Common/CnConsts.pas
@@ -71,7 +71,7 @@ var
 const
   // CnPack Information
   SCnPackAbout = 'CnPack';
-  SCnPackVer = 'Ver 0.1.0.2';
+  SCnPackVer = 'Ver 0.1.0.3';
   SCnPackStr = SCnPackAbout + ' ' + SCnPackVer;
   SCnPackUrl = 'http://www.cnpack.org';
   SCnPackBbsUrl = 'http://bbs.cnpack.org';
