9d52c910283911e95b9be91a93534a9280ea6e5b
liuxiao <liuxiao@tanplay.com>
2015-05-16 23:00:20 +0800

    * [Graphics] Add a Splitter to Allow Setting Control.

---
+++ Source/Graphics/CnSplitter.pas
@@ -0,0 +1,446 @@
+{******************************************************************************}
+{                       CnPack For Delphi/C++Builder                           }
+{                     中国人自己的开放源码第三方开发包                         }
+{                   (C)Copyright 2001-2015 CnPack 开发组                       }
+{                   ------------------------------------                       }
+{                                                                              }
+{            本开发包是开源的自由软件，您可以遵照 CnPack 的发布协议来修        }
+{        改和重新发布这一程序。                                                }
+{                                                                              }
+{            发布这一开发包的目的是希望它有用，但没有任何担保。甚至没有        }
+{        适合特定目的而隐含的担保。更详细的情况请参阅 CnPack 发布协议。        }
+{                                                                              }
+{            您应该已经和开发包一起收到一份 CnPack 发布协议的副本。如果        }
+{        还没有，可访问我们的网站：                                            }
+{                                                                              }
+{            网站地址：http://www.cnpack.org                                   }
+{            电子邮件：master@cnpack.org                                       }
+{                                                                              }
+{******************************************************************************}
+
+unit CnSplitter;
+{* |<PRE>
+================================================================================
+* 软件名称：界面控件包
+* 单元名称：界面控件包分隔条实现单元
+* 单元作者：LiuXiao
+* 备    注：本分隔条移植自 D5 的 TSplitter 并允许外部设置被拖动条控制的 Control
+* 开发平台：PWinXP + Delphi 5.0
+* 兼容测试：PWinXP/7 + Delphi XE7
+* 本 地 化：该单元中的字符串均符合本地化处理方式
+* 单元标识：$Id$
+* 修改记录：2015.05.16 V0.1
+*               移植并实现单元
+================================================================================
+|</PRE>}
+
+interface
+
+{$I CnPack.inc}
+
+uses
+  SysUtils, Classes, Controls, Graphics, Windows, Messages, Forms, ExtCtrls;
+
+type
+  TCnSplitter = class(TGraphicControl)
+  private
+    FActiveControl: TWinControl;
+    FAutoSnap: Boolean;
+    FBeveled: Boolean;
+    FBrush: TBrush;
+    FControl: TControl;
+    FDownPos: TPoint;
+    FLineDC: HDC;
+    FLineVisible: Boolean;
+    FMinSize: NaturalNumber;
+    FMaxSize: Integer;
+    FNewSize: Integer;
+    FOldKeyDown: TKeyEvent;
+    FOldSize: Integer;
+    FPrevBrush: HBrush;
+    FResizeStyle: TResizeStyle;
+    FSplit: Integer;
+    FOnCanResize: TCanResizeEvent;
+    FOnMoved: TNotifyEvent;
+    FOnPaint: TNotifyEvent;
+    FSplitControl: TControl;
+    procedure AllocateLineDC;
+    procedure CalcSplitSize(X, Y: Integer; var NewSize, Split: Integer);
+    procedure DrawLine;
+    function FindControl: TControl;
+    procedure FocusKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure ReleaseLineDC;
+    procedure SetBeveled(Value: Boolean);
+    procedure UpdateControlSize;
+    procedure UpdateSize(X, Y: Integer);
+  protected
+    function CanResize(var NewSize: Integer): Boolean; reintroduce; virtual;
+    function DoCanResize(var NewSize: Integer): Boolean; virtual;
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState;
+      X, Y: Integer); override;
+    procedure MouseMove(Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState;
+      X, Y: Integer); override;
+    procedure Paint; override;
+    procedure RequestAlign; override;
+    procedure StopSizing; dynamic;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    property Canvas;
+  published
+    property Align default alLeft;
+    property AutoSnap: Boolean read FAutoSnap write FAutoSnap default True;
+    property Beveled: Boolean read FBeveled write SetBeveled default False;
+    property Color;
+    property Constraints;
+    property MinSize: NaturalNumber read FMinSize write FMinSize default 30;
+    property ParentColor;
+    property ResizeStyle: TResizeStyle read FResizeStyle write FResizeStyle
+      default rsPattern;
+    property SplitControl: TControl read FSplitControl write FSplitControl;
+    property Visible;
+    property OnCanResize: TCanResizeEvent read FOnCanResize write FOnCanResize;
+    property OnMoved: TNotifyEvent read FOnMoved write FOnMoved;
+    property OnPaint: TNotifyEvent read FOnPaint write FOnPaint;
+  end;
+
+implementation
+
+{ TCnSplitter }
+
+type
+  TWinControlAccess = class(TWinControl);
+
+constructor TCnSplitter.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FAutoSnap := True;
+  Align := alLeft;
+  Width := 3;
+  Cursor := crHSplit;
+  FMinSize := 30;
+  FResizeStyle := rsPattern;
+  FOldSize := -1;
+end;
+
+destructor TCnSplitter.Destroy;
+begin
+  FBrush.Free;
+  inherited Destroy;
+end;
+
+procedure TCnSplitter.AllocateLineDC;
+begin
+  FLineDC := GetDCEx(Parent.Handle, 0, DCX_CACHE or DCX_CLIPSIBLINGS
+    or DCX_LOCKWINDOWUPDATE);
+  if ResizeStyle = rsPattern then
+  begin
+    if FBrush = nil then
+    begin
+      FBrush := TBrush.Create;
+      FBrush.Bitmap := AllocPatternBitmap(clBlack, clWhite);
+    end;
+    FPrevBrush := SelectObject(FLineDC, FBrush.Handle);
+  end;
+end;
+
+procedure TCnSplitter.DrawLine;
+var
+  P: TPoint;
+begin
+  FLineVisible := not FLineVisible;
+  P := Point(Left, Top);
+  if Align in [alLeft, alRight] then
+    P.X := Left + FSplit else
+    P.Y := Top + FSplit;
+  with P do PatBlt(FLineDC, X, Y, Width, Height, PATINVERT);
+end;
+
+procedure TCnSplitter.ReleaseLineDC;
+begin
+  if FPrevBrush <> 0 then
+    SelectObject(FLineDC, FPrevBrush);
+  ReleaseDC(Parent.Handle, FLineDC);
+  if FBrush <> nil then
+  begin
+    FBrush.Free;
+    FBrush := nil;
+  end;
+end;
+
+function TCnSplitter.FindControl: TControl;
+var
+  P: TPoint;
+  I: Integer;
+  R: TRect;
+begin
+  Result := nil;
+  P := Point(Left, Top);
+  case Align of
+    alLeft: Dec(P.X);
+    alRight: Inc(P.X, Width);
+    alTop: Dec(P.Y);
+    alBottom: Inc(P.Y, Height);
+  else
+    Exit;
+  end;
+  for I := 0 to Parent.ControlCount - 1 do
+  begin
+    Result := Parent.Controls[I];
+    if Result.Visible and Result.Enabled then
+    begin
+      R := Result.BoundsRect;
+      if (R.Right - R.Left) = 0 then
+        if Align in [alTop, alLeft] then
+          Dec(R.Left)
+        else
+          Inc(R.Right);
+      if (R.Bottom - R.Top) = 0 then
+        if Align in [alTop, alLeft] then
+          Dec(R.Top)
+        else
+          Inc(R.Bottom);
+      if PtInRect(R, P) then Exit;
+    end;
+  end;
+  Result := nil;
+end;
+
+procedure TCnSplitter.RequestAlign;
+begin
+  inherited RequestAlign;
+  if (Cursor <> crVSplit) and (Cursor <> crHSplit) then Exit;
+  if Align in [alBottom, alTop] then
+    Cursor := crVSplit
+  else
+    Cursor := crHSplit;
+end;
+
+procedure TCnSplitter.Paint;
+const
+  XorColor = $00FFD8CE;
+var
+  FrameBrush: HBRUSH;
+  R: TRect;
+begin
+  R := ClientRect;
+  Canvas.Brush.Color := Color;
+  Canvas.FillRect(ClientRect);
+  if Beveled then
+  begin
+    if Align in [alLeft, alRight] then
+      InflateRect(R, -1, 2) else
+      InflateRect(R, 2, -1);
+    OffsetRect(R, 1, 1);
+    FrameBrush := CreateSolidBrush(ColorToRGB(clBtnHighlight));
+    FrameRect(Canvas.Handle, R, FrameBrush);
+    DeleteObject(FrameBrush);
+    OffsetRect(R, -2, -2);
+    FrameBrush := CreateSolidBrush(ColorToRGB(clBtnShadow));
+    FrameRect(Canvas.Handle, R, FrameBrush);
+    DeleteObject(FrameBrush);
+  end;
+  if csDesigning in ComponentState then
+    { Draw outline }
+    with Canvas do
+    begin
+      Pen.Style := psDot;
+      Pen.Mode := pmXor;
+      Pen.Color := XorColor;
+      Brush.Style := bsClear;
+      Rectangle(0, 0, ClientWidth, ClientHeight);
+    end;
+  if Assigned(FOnPaint) then FOnPaint(Self);
+end;
+
+function TCnSplitter.DoCanResize(var NewSize: Integer): Boolean;
+begin
+  Result := CanResize(NewSize);
+  if Result and (NewSize <= MinSize) and FAutoSnap then
+    NewSize := 0;
+end;
+
+function TCnSplitter.CanResize(var NewSize: Integer): Boolean;
+begin
+  Result := True;
+  if Assigned(FOnCanResize) then FOnCanResize(Self, NewSize, Result);
+end;
+
+procedure TCnSplitter.MouseDown(Button: TMouseButton; Shift: TShiftState;
+  X, Y: Integer);
+var
+  I: Integer;
+begin
+  inherited MouseDown(Button, Shift, X, Y);
+  if Button = mbLeft then
+  begin
+    if FSplitControl = nil then
+      FControl := FindControl
+    else
+      FControl := FSplitControl;
+    FDownPos := Point(X, Y);
+    if Assigned(FControl) then
+    begin
+      if Align in [alLeft, alRight] then
+      begin
+        FMaxSize := Parent.ClientWidth - FMinSize;
+        for I := 0 to Parent.ControlCount - 1 do
+          with Parent.Controls[I] do
+            if Align in [alLeft, alRight] then Dec(FMaxSize, Width);
+        Inc(FMaxSize, FControl.Width);
+      end
+      else
+      begin
+        FMaxSize := Parent.ClientHeight - FMinSize;
+        for I := 0 to Parent.ControlCount - 1 do
+          with Parent.Controls[I] do
+            if Align in [alTop, alBottom] then Dec(FMaxSize, Height);
+        Inc(FMaxSize, FControl.Height);
+      end;
+      UpdateSize(X, Y);
+      AllocateLineDC;
+      with ValidParentForm(Self) do
+        if ActiveControl <> nil then
+        begin
+          FActiveControl := ActiveControl;
+          FOldKeyDown := TWinControlAccess(FActiveControl).OnKeyDown;
+          TWinControlAccess(FActiveControl).OnKeyDown := FocusKeyDown;
+        end;
+      if ResizeStyle in [rsLine, rsPattern] then DrawLine;
+    end;
+  end;
+end;
+
+procedure TCnSplitter.UpdateControlSize;
+begin
+  if FNewSize <> FOldSize then
+  begin
+    case Align of
+      alLeft: FControl.Width := FNewSize;
+      alTop: FControl.Height := FNewSize;
+      alRight:
+        begin
+          Parent.DisableAlign;
+          try
+            FControl.Left := FControl.Left + (FControl.Width - FNewSize);
+            FControl.Width := FNewSize;
+          finally
+            Parent.EnableAlign;
+          end;
+        end;
+      alBottom:
+        begin
+          Parent.DisableAlign;
+          try
+            FControl.Top := FControl.Top + (FControl.Height - FNewSize);
+            FControl.Height := FNewSize;
+          finally
+            Parent.EnableAlign;
+          end;
+        end;
+    end;
+    Update;
+    if Assigned(FOnMoved) then FOnMoved(Self);
+    FOldSize := FNewSize;
+  end;
+end;
+
+procedure TCnSplitter.CalcSplitSize(X, Y: Integer; var NewSize, Split: Integer);
+var
+  S: Integer;
+begin
+  if Align in [alLeft, alRight] then
+    Split := X - FDownPos.X
+  else
+    Split := Y - FDownPos.Y;
+  S := 0;
+  case Align of
+    alLeft: S := FControl.Width + Split;
+    alRight: S := FControl.Width - Split;
+    alTop: S := FControl.Height + Split;
+    alBottom: S := FControl.Height - Split;
+  end;
+  NewSize := S;
+  if S < FMinSize then
+    NewSize := FMinSize
+  else if S > FMaxSize then
+    NewSize := FMaxSize;
+  if S <> NewSize then
+  begin
+    if Align in [alRight, alBottom] then
+      S := S - NewSize else
+      S := NewSize - S;
+    Inc(Split, S);
+  end;
+end;
+
+procedure TCnSplitter.UpdateSize(X, Y: Integer);
+begin
+  CalcSplitSize(X, Y, FNewSize, FSplit);
+end;
+
+procedure TCnSplitter.MouseMove(Shift: TShiftState; X, Y: Integer);
+var
+  NewSize, Split: Integer;
+begin
+  inherited;
+  if (ssLeft in Shift) and Assigned(FControl) then
+  begin
+    CalcSplitSize(X, Y, NewSize, Split);
+    if DoCanResize(NewSize) then
+    begin
+      if ResizeStyle in [rsLine, rsPattern] then DrawLine;
+      FNewSize := NewSize;
+      FSplit := Split;
+      if ResizeStyle = rsUpdate then UpdateControlSize;
+      if ResizeStyle in [rsLine, rsPattern] then DrawLine;
+    end;
+  end;
+end;
+
+procedure TCnSplitter.MouseUp(Button: TMouseButton; Shift: TShiftState;
+  X, Y: Integer);
+begin
+  inherited;
+  if Assigned(FControl) then
+  begin
+    if ResizeStyle in [rsLine, rsPattern] then DrawLine;
+    UpdateControlSize;
+    StopSizing;
+  end;
+end;
+
+procedure TCnSplitter.FocusKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if Key = VK_ESCAPE then
+    StopSizing
+  else if Assigned(FOldKeyDown) then
+    FOldKeyDown(Sender, Key, Shift);
+end;
+
+procedure TCnSplitter.SetBeveled(Value: Boolean);
+begin
+  FBeveled := Value;
+  Repaint;
+end;
+
+procedure TCnSplitter.StopSizing;
+begin
+  if Assigned(FControl) then
+  begin
+    if FLineVisible then DrawLine;
+    FControl := nil;
+    ReleaseLineDC;
+    if Assigned(FActiveControl) then
+    begin
+      TWinControlAccess(FActiveControl).OnKeyDown := FOldKeyDown;
+      FActiveControl := nil;
+    end;
+  end;
+  if Assigned(FOnMoved) then
+    FOnMoved(Self);
+end;
+
+end.
+ 
