9deafee235636a6f449fc075b8dcfd64cbfbc64d
liuxiao <liuxiao@tanplay.com>
2015-08-07 22:56:40 +0800

    * [Readme] Change Version to 1.0.7.

--- Source/Common/CnConsts.pas
+++ Source/Common/CnConsts.pas
@@ -71,7 +71,7 @@ var
 const
   // CnPack Information
   SCnPackAbout = 'CnPack';
-  SCnPackVer = 'Ver 0.1.0.6';
+  SCnPackVer = 'Ver 0.1.0.7';
   SCnPackStr = SCnPackAbout + ' ' + SCnPackVer;
   SCnPackUrl = 'http://www.cnpack.org';
   SCnPackBbsUrl = 'http://bbs.cnpack.org';
