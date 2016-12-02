30ac9270b62161186a964700234c1769d4a6b9b0
liuxiao <liuxiao@cnpack.org>
2016-12-02 13:42:54 +0800

    + [Queue] Add an Object Stack.

--- Source/Common/CnQueue.pas
+++ Source/Common/CnQueue.pas
@@ -55,7 +55,9 @@ unit CnQueue;
 * 兼容测试：PWin2000/XP + Delphi 5/6/7
 * 本 地 化：该单元中的字符串均符合本地化处理方式
 * 单元标识：$Id$
-* 修改记录：2008.04.30 V1.0
+* 修改记录：2016.12.02 V1.1
+*               加入 TCnObjectStack 实现，允许 Clear 等方法
+*           2008.04.30 V1.0
 *               小峰从原始代码移植而来。
 ================================================================================
 |</PRE>}
@@ -65,7 +67,7 @@ interface
 {$I CnPack.inc}
 
 uses
-  Windows, SysUtils;
+  Windows, SysUtils, Classes;
 
 type
   TCnQueue = class
@@ -82,6 +84,22 @@ type
     procedure Push(Data: Pointer);
     function Pop: Pointer;
     property Size: Integer read GetSize;
+  end;
+
+  TCnObjectStack = class(TObject)
+  private
+    FList: TList;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    function Count: Integer;
+    function IsEmpty: Boolean;
+    procedure Clear;
+
+    procedure Push(AObject: TObject);
+    function Pop: TObject;
+    function Peek: TObject;
   end;
 
 implementation
@@ -179,6 +197,50 @@ end;
 function TCnQueue.GetSize: Integer;
 begin
   Result := FSize;
+end;
+
+{ TCnObjectStack }
+
+procedure TCnObjectStack.Clear;
+begin
+  FList.Clear;
+end;
+
+function TCnObjectStack.Count: Integer;
+begin
+  Result := FList.Count;
+end;
+
+constructor TCnObjectStack.Create;
+begin
+  FList := TList.Create;
+end;
+
+destructor TCnObjectStack.Destroy;
+begin
+  FList.Free;
+  inherited;
+end;
+
+function TCnObjectStack.IsEmpty: Boolean;
+begin
+  Result := FList.Count = 0;
+end;
+
+function TCnObjectStack.Peek: TObject;
+begin
+  Result := TObject(FList[FList.Count - 1]);
+end;
+
+function TCnObjectStack.Pop: TObject;
+begin
+  Result := TObject(FList[FList.Count - 1]);
+  FList.Delete(FList.Count - 1);
+end;
+
+procedure TCnObjectStack.Push(AObject: TObject);
+begin
+  FList.Add(AObject);
 end;
 
 end.
