439352ae180a9f53407135eb68b77a5f3acf6ba3
liuxiao <liuxiao@cnpack.org>
2017-05-01 09:52:09 +0800

    * [CnConsts] Change Version to 0.1.1.3.

--- Source/Common/CnConsts.pas
+++ Source/Common/CnConsts.pas
@@ -71,7 +71,7 @@ var
 const
   // CnPack Information
   SCnPackAbout = 'CnPack';
-  SCnPackVer = 'Ver 0.1.1.2';
+  SCnPackVer = 'Ver 0.1.1.3';
   SCnPackStr = SCnPackAbout + ' ' + SCnPackVer;
   SCnPackUrl = 'http://www.cnpack.org';
   SCnPackBbsUrl = 'http://bbs.cnpack.org';
