7bea31c13e3929923bfdbd2038f515a877395da8
liuxiao <liuxiao@cnpack.org>
2018-06-05 16:25:56 +0800

    * [Native Declarations] Add UInt64 div and mod Routine to Non-UInt64 Platform.

---
+++ Examples/NativeDecl/UnitNative.dfm
@@ -0,0 +1,53 @@
+object FormNative: TFormNative
+  Left = 192
+  Top = 107
+  BorderStyle = bsDialog
+  Caption = 
+    'Test some Native Declarations and Methods - Run on D567 and can ' +
+    'be verified in BDS.'
+  ClientHeight = 399
+  ClientWidth = 614
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object btnUInt64Div: TButton
+    Left = 16
+    Top = 16
+    Width = 75
+    Height = 25
+    Caption = 'UInt64 Div'
+    TabOrder = 0
+    OnClick = btnUInt64DivClick
+  end
+  object btnUInt64Mod: TButton
+    Left = 520
+    Top = 16
+    Width = 75
+    Height = 25
+    Caption = 'UInt64 Mod'
+    TabOrder = 1
+    OnClick = btnUInt64ModClick
+  end
+  object mmoRes: TMemo
+    Left = 16
+    Top = 64
+    Width = 577
+    Height = 316
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = 'Courier New'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 2
+  end
+end
--- Source/Common/CnNativeDecl.pas
+++ Source/Common/CnNativeDecl.pas
@@ -29,12 +29,15 @@ unit CnNativeDecl;
 *           考虑到兼容性，固定长度的 32 位 Cardinal/Integer 等和 Pointer 这些就
 *           不能再通用了，即使 32 位下也被编译器禁止。因此本单元声明了几个类型，
 *           供同时在低版本和高版本的 Delphi 中使用。
-*           后来加入 UInt64 的包装，注意 D567 下不支持UInt64 的运算。
+*           后来加入 UInt64 的包装，注意 D567 下不直接支持UInt64 的运算，需要用
+*           辅助函数实现，目前实现了 div 与 mod
 * 开发平台：PWin2000 + Delphi 5.0
 * 兼容测试：PWin9X/2000/XP + Delphi 5/6/7 XE 2
 * 本 地 化：该单元中的字符串均符合本地化处理方式
 * 单元标识：$Id: CnNativeDecl.pas 761 2011-02-07 14:08:58Z liuxiao@cnpack.org $
-* 修改记录：2016.09.27 V1.1
+* 修改记录：2018.06.05 V1.2
+*               加入 64 位整型的 div/mod 运算，在不支持 UInt64 的系统上以 Int64 代替 
+*           2016.09.27 V1.1
 *               加入 64 位整型的一些定义
 *           2011.07.06 V1.0
 *               创建单元，实现功能
@@ -75,6 +78,68 @@ type
   TCnInt64         = Int64;
 {$ENDIF}
 
+// TUInt64 用于 cnvcl 库中不支持 UInt64 的运算如 div mod 等
+{$IFDEF SUPPORT_UINT64}
+  TUInt64          = UInt64;
+{$ELSE}
+  TUInt64          = Int64;
+{$ENDIF}
+
+{*
+  对于 D567 等不支持 UInt64 的编译器，虽然可以用 Int64 代替 UInt64 进行加减、存储
+  但乘除运算则无法直接完成，这里封装了两个调用 System 库中的 _lludiv 与 _llumod
+  函数，实现以 Int64 表示的 UInt64 数据的 div 与 mod 功能。
+}
+function UInt64Mod(A, B: TUInt64): TUInt64;
+
+function UInt64Div(A, B: TUInt64): TUInt64;
+
+function UInt64ToStr(N: TUInt64): string;
+
+function StrToUInt64(const S: string): TUInt64;
+
 implementation
+
+{
+  UInt64 求 A mod B
+
+  调用的入栈顺序是 A 的高位，A 的低位，B 的高位，B 的低位。挨个 push 完毕并进入函数后，
+  ESP 是返回地址，ESP+4 是 B 的低位，ESP + 8 是 B 的高位，ESP + C 是 A 的低位，ESP + 10 是 A 的高位
+  进入后 push esp 让 ESP 减了 4，然后 mov ebp esp，之后用 EBP 来寻址，全要多加 4
+
+  而 System.@_llumod 要求在刚进入时，EAX <- A 的低位，EDX <- A 的高位，（System 源码注释中 EAX/EDX 写反了）
+  [ESP + 8]（也就是 EBP + C）<- B 的高位，[ESP + 4] （也就是 EBP + 8）<- B 的低位
+
+  所以 CALL 前加了四句搬移代码
+}
+function UInt64Mod(A, B: TUInt64): TUInt64;
+asm
+        // PUSH ESP 让 ESP 减了 4，要补上
+        MOV     EAX, [EBP + $10]              // A Lo
+        MOV     EDX, [EBP + $14]              // A Hi
+        PUSH    DWORD PTR[EBP + $C]           // B Hi
+        PUSH    DWORD PTR[EBP + $8]           // B Lo
+        CALL    System.@_llumod;
+end;
+
+function UInt64Div(A, B: TUInt64): TUInt64;
+asm
+        // PUSH ESP 让 ESP 减了 4，要补上
+        MOV     EAX, [EBP + $10]              // A Lo
+        MOV     EDX, [EBP + $14]              // A Hi
+        PUSH    DWORD PTR[EBP + $C]           // B Hi
+        PUSH    DWORD PTR[EBP + $8]           // B Lo
+        CALL    System.@_lludiv;
+end;
+
+function UInt64ToStr(N: TUInt64): string;
+begin
+  Result := Format('%u', [N]);
+end;
+
+function StrToUInt64(const S: string): TUInt64;
+begin
+  // Not Implemented
+end;
 
 end.
