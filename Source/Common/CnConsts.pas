913316288988bebaff0ba9074d872d956adb8e3d
LiuXiao <liuxiao@cnpack.org>
2020-03-11 20:36:46 +0800

    * [Readme] Change Version to 1.1.9.

--- Source/Common/CnConsts.pas
+++ Source/Common/CnConsts.pas
@@ -70,7 +70,7 @@ var
 const
   // CnPack Information
   SCnPackAbout = 'CnPack';
-  SCnPackVer = 'Ver 0.1.1.8';
+  SCnPackVer = 'Ver 0.1.1.9';
   SCnPackStr = SCnPackAbout + ' ' + SCnPackVer;
   SCnPackUrl = 'http://www.cnpack.org';
   SCnPackBbsUrl = 'http://bbs.cnpack.org';
