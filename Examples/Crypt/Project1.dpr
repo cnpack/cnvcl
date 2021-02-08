102cfb80b63df4d7307a197d7b558c36495ff85f
Liu Xiao <liuxiao@cnpack.org>
2021-02-08 18:27:51 +0800

    * [Packages] Update D5 Package and Crypto Demo.

--- Examples/Crypt/Project1.dpr
+++ Examples/Crypt/Project1.dpr
@@ -3,18 +3,18 @@ program Project1;
 uses
   Forms,
   Unit1 in 'Unit1.pas' {FormCrypt},
-  CnMD5 in '..\..\Source\Common\CnMD5.pas',
-  CnBase64 in '..\..\Source\Common\CnBase64.pas',
-  CnCRC32 in '..\..\Source\Common\CnCRC32.pas',
-  CnDES in '..\..\Source\Common\CnDES.pas',
-  CnSHA1 in '..\..\Source\Common\CnSHA1.pas',
-  CnSM3 in '..\..\Source\Common\CnSM3.pas',
-  CnSM4 in '..\..\Source\Common\CnSM4.pas',
-  CnAES in '..\..\Source\Common\CnAES.pas',
-  CnSHA2 in '..\..\Source\Common\CnSHA2.pas',
-  CnZUC in '..\..\Source\Common\CnZUC.pas',
-  CnSHA3 in '..\..\Source\Common\CnSHA3.pas',
-  CnTEA in '..\..\Source\Common\CnTEA.pas';
+  CnMD5 in '..\..\Source\Crypto\CnMD5.pas',
+  CnBase64 in '..\..\Source\Crypto\CnBase64.pas',
+  CnCRC32 in '..\..\Source\Crypto\CnCRC32.pas',
+  CnDES in '..\..\Source\Crypto\CnDES.pas',
+  CnSHA1 in '..\..\Source\Crypto\CnSHA1.pas',
+  CnSM3 in '..\..\Source\Crypto\CnSM3.pas',
+  CnSM4 in '..\..\Source\Crypto\CnSM4.pas',
+  CnAES in '..\..\Source\Crypto\CnAES.pas',
+  CnSHA2 in '..\..\Source\Crypto\CnSHA2.pas',
+  CnZUC in '..\..\Source\Crypto\CnZUC.pas',
+  CnSHA3 in '..\..\Source\Crypto\CnSHA3.pas',
+  CnTEA in '..\..\Source\Crypto\CnTEA.pas';
 
 {$R *.RES}
 
