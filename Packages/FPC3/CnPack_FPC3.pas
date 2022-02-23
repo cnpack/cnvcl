f89c25b19d9fe225c05d785a21c0a050492324fa
Liu Xiao <liuxiao@cnpack.org>
2022-02-23 17:28:30 +0800

    # [Packages] Add a FPC3 Package just for some Units Compiling.

---
+++ Packages/FPC3/CnPack_FPC3.pas
@@ -0,0 +1,23 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit CnPack_FPC3;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  CnAES, CnBase64, CnBerUtils, CnBigNumber, CnCertificateAuthority, CnComplex, 
+  CnCRC32, CnDES, CnDFT, CnECC, CnFEC, CnKDF, CnMD5, CnNativeDecl, CnOTP, 
+  CnPemUtils, CnPolynomial, CnPrimeNumber, CnRandom, CnRSA, CnSHA1, CnSHA2, 
+  CnSHA3, CnSM2, CnSM3, CnSM4, CnSM9, CnTEA, CnZUC, CnLinkedList, 
+  CnBigDecimal, CnBigRational, CnBloomFilter, CnCalClass, CnCalendar, 
+  CnClasses, CnConsts, CnContainers, CnDancingLinks, CnFitCurve, 
+  CnFloatConvert, CnGraph, CnGraphUtils, CnHashMap, CnHashTable, CnLockFree, 
+  CnMath, CnMatrix, CnRopes, CnSingleton, CnSingletonComp, CnSkipList, 
+  CnStrDiff, CnStream, CnStrings, CnTree, CnTreeClasses, CnWideStrings;
+
+implementation
+
+end.
