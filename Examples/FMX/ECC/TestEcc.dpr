fabc7bfdd9df2ecf2fdea80c3ec2eb99c11cce89
Liu Xiao <liuxiao@cnpack.org>
2022-05-10 09:02:48 +0800

    * [ECC] Adjust FMX Demo Project.

--- Examples/FMX/ECC/TestEcc.dpr
+++ Examples/FMX/ECC/TestEcc.dpr
@@ -2,11 +2,13 @@ program TestEcc;
 
 uses
   System.StartUpCopy,
-  FMX.Forms;
+  FMX.Forms,
+  UnitEcc in 'UnitEcc.pas' {FormEcc};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TFormEcc, FormEcc);
   Application.Run;
 end.
