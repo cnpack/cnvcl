81c38294eb7255d300f547770c655fc9ad42184c
Liu Xiao <liuxiao@cnpack.org>
2022-11-25 22:16:55 +0800

    * [GB18030] Move Some Unicode Routines to CnWideStrings.

--- Source/Common/CnGB18030.pas
+++ Source/Common/CnGB18030.pas
@@ -152,7 +152,9 @@ unit CnGB18030;
 * 开发平台：PWin98SE + Delphi 5.0
 * 兼容测试：PWin9X/2000/XP + Delphi 5/6
 * 本 地 化：该单元中的字符串均符合本地化处理方式
-* 修改记录：2022.11.20
+* 修改记录：2022.11.25
+*               将部分 Unicode 相关函数移至 CnWideStrings 中
+*           2022.11.20
 *               实现 52 个重码字的判断
 *           2022.11.16
 *               实现不依赖于 Windows API 的 Unicode 字符到 GB18030-2022 的转换
@@ -167,17 +169,11 @@ interface
 
 {$I CnPack.inc}
 
-// {$DEFINE UTF16_BE}
-
-// Delphi 默认 UTF16-LE，如果要处理 UTF16-BE 字符串，需要定义 UTF16_BE
-
 uses
-  SysUtils, Classes, CnNative;
+  SysUtils, Classes, CnNative, CnWideStrings;
 
 const
   CN_GB18030_CODEPAGE  = 54936;
-  CN_INVALID_CODEPOINT = $FFFFFFFF;
-  CN_ALTERNATIVE_CHAR  = '?';
 
 type
 {$IFDEF SUPPORT_ANSISTRING_CODEPAGE}
@@ -193,39 +189,8 @@ type
   PCnGB18030StringPtr = PAnsiChar;
   {* GB18030 编码的字符指针，内部用 PAnsiChar 表示}
 
-  TCnCodePoint = type Cardinal;
-  {* 字符码值，或者叫码点，不等于表达的编码方式}
-
-  TCn2CharRec = packed record
-  {* 双字节字符结构}
-    P1: AnsiChar;
-    P2: AnsiChar;
-  end;
-  PCn2CharRec = ^TCn2CharRec;
-
-  TCn4CharRec = packed record
-  {* 四字节字符结构}
-    P1: AnsiChar;
-    P2: AnsiChar;
-    P3: AnsiChar;
-    P4: AnsiChar;
-  end;
-  PCn4CharRec = ^TCn4CharRec;
-
-function GetCharLengthFromUtf8(Utf8Str: PAnsiChar): Integer;
-{* 计算一 UTF8（可能是 UTF8-MB4）字符串的字符数}
-
-function GetCharLengthFromUtf16(Utf16Str: PWideChar): Integer;
-{* 计算一 UTF16（可能混合 Unicode 扩展平面里的四字节字符）字符串的字符数}
-
 function GetCharLengthFromGB18030(GB18030Str: PCnGB18030StringPtr): Integer;
 {* 计算一 GB18030 字符串的字符数}
-
-function GetByteWidthFromUtf8(Utf8Str: PAnsiChar): Integer;
-{* 计算一 UTF8（可能是 UTF8-MB4）字符串的当前字符占多少字节}
-
-function GetByteWidthFromUtf16(Utf16Str: PWideChar): Integer;
-{* 计算一 UTF16（可能混合 Unicode 扩展平面里的四字节字符）字符串的当前字符占多少字节}
 
 function GetByteWidthFromGB18030(GB18030Str: PCnGB18030StringPtr): Integer;
 {* 计算一 GB18030 字符串的当前字符占多少字节}
@@ -260,18 +225,6 @@ function GetUtf16FromGB18030(GB18030Str: TCnGB18030String): WideString;
 {* 返回一 GB18030 字符串对应的 Utf16 字符串}
 
 {$ENDIF}
-
-function GetCodePointFromUtf16Char(Utf16Str: PWideChar): TCnCodePoint;
-{* 计算一个 Utf16 字符的编码值（也叫代码位置），注意 Utf16Str 可能指向一个双字节字符，也可能指向一个四字节字符}
-
-function GetCodePointFromUtf164Char(PtrTo4Char: Pointer): TCnCodePoint;
-{* 计算一个四字节 Utf16 字符的编码值（也叫代码位置）}
-
-function GetUtf16CharFromCodePoint(CP: TCnCodePoint; PtrToChars: Pointer): Integer;
-{* 计算一个 Unicode 编码值的二字节或四字节表示，如果 PtrToChars 指向的位置不为空，
-  则将结果放在 PtrToChars 所指的二字节或四字节区域
-  调用者在 CP 超过 $FFFF 时须保证 PtrToChars 所指的区域至少四字节，反之二字节即可
-  返回 1 或 2，分别表示处理的是二字节或四字节}
 
 function GetCodePointFromGB18030Char(PtrToGB18030Chars: PCnGB18030StringPtr): TCnCodePoint;
 {* 计算一个 GB18030 字符的编码值（也叫代码位置），注意 PtrToGB18030Chars 可能指向一个单、双、四字节字符}
@@ -338,18 +291,6 @@ function GetNextGB18030CodePoint(CP: TCnCodePoint; CheckRange: Boolean = False):
   CheckRange 为 True 时表示会严格检查 CP 是否合法的 GB18030 编码值，是才返回后一个
   为 False 时无论该字符是否合法，均返回其后一个合法字符编码值}
 
-function GetUtf16HighByte(Rec: PCn2CharRec): Byte; {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
-{* 得到一个 UTF 16 双字节字符的高位字节值}
-
-function GetUtf16LowByte(Rec: PCn2CharRec): Byte; {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
-{* 得到一个 UTF 16 双字节字符的低位字节值}
-
-procedure SetUtf16HighByte(B: Byte; Rec: PCn2CharRec); {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
-{* 设置一个 UTF 16 双字节字符的高位字节值}
-
-procedure SetUtf16LowByte(B: Byte; Rec: PCn2CharRec); {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
-{* 设置一个 UTF 16 双字节字符的低位字节值}
-
 implementation
 
 uses
@@ -365,16 +306,6 @@ type
   end;
 
 const
-  CN_UTF16_4CHAR_PREFIX1_LOW  = $D8;
-  CN_UTF16_4CHAR_PREFIX1_HIGH = $DC;
-  CN_UTF16_4CHAR_PREFIX2_LOW  = $DC;
-  CN_UTF16_4CHAR_PREFIX2_HIGH = $E0;
-
-  CN_UTF16_4CHAR_HIGH_MASK    = $3;
-  CN_UTF16_4CHAR_SPLIT_MASK   = $3FF;
-
-  CN_UTF16_EXT_BASE           = $10000;
-
   CN_GB18030_BOM: array[0..3] of Byte = ($84, $31, $95, $33);
 
   // GB18030 中的 52 个重码字的编码值，源于 GBK，原始数据在双字节四区，现在扩展区
@@ -947,68 +878,6 @@ begin
   end;
 end;
 
-function GetUtf16HighByte(Rec: PCn2CharRec): Byte;
-begin
-{$IFDEF UTF16_BE}
-  Result := Byte(Rec^.P1);
-{$ELSE}
-  Result := Byte(Rec^.P2); // UTF16-LE 的高低位会置换
-{$ENDIF}
-end;
-
-function GetUtf16LowByte(Rec: PCn2CharRec): Byte;
-begin
-{$IFDEF UTF16_BE}
-  Result := Byte(Rec^.P2);
-{$ELSE}
-  Result := Byte(Rec^.P1); // UTF16-LE 的高低位会置换
-{$ENDIF}
-end;
-
-procedure SetUtf16HighByte(B: Byte; Rec: PCn2CharRec);
-begin
-{$IFDEF UTF16_BE}
-  Rec^.P1 := AnsiChar(B);
-{$ELSE}
-  Rec^.P2 := AnsiChar(B); // UTF16-LE 的高低位会置换
-{$ENDIF}
-end;
-
-procedure SetUtf16LowByte(B: Byte; Rec: PCn2CharRec);
-begin
-{$IFDEF UTF16_BE}
-  Rec^.P2 := AnsiChar(B);
-{$ELSE}
-  Rec^.P1 := AnsiChar(B); // UTF16-LE 的高低位会置换
-{$ENDIF}
-end;
-
-function GetCharLengthFromUtf8(Utf8Str: PAnsiChar): Integer;
-var
-  L: Integer;
-begin
-  Result := 0;
-  while Utf8Str^ <> #0 do
-  begin
-    L := GetByteWidthFromUtf8(Utf8Str);
-    Inc(Utf8Str, L);
-    Inc(Result);
-  end;
-end;
-
-function GetCharLengthFromUtf16(Utf16Str: PWideChar): Integer;
-var
-  L: Integer;
-begin
-  Result := 0;
-  while Utf16Str^ <> #0 do
-  begin
-    L := GetByteWidthFromUtf16(Utf16Str);
-    Utf16Str := PWideChar(TCnNativeInt(Utf16Str) + L);
-    Inc(Result);
-  end;
-end;
-
 function GetCharLengthFromGB18030(GB18030Str: PCnGB18030StringPtr): Integer;
 var
   L: Integer;
@@ -1019,49 +888,6 @@ begin
     L := GetByteWidthFromGB18030(GB18030Str);
     Inc(GB18030Str, L);
     Inc(Result);
-  end;
-end;
-
-function GetByteWidthFromUtf8(Utf8Str: PAnsiChar): Integer;
-var
-  B: Byte;
-begin
-  B := Byte(Utf8Str^);
-  if B >= $FC then        // 6 个 1，1 个 0，先不考虑七或八 1 的情况
-    Result := 6
-  else if B >= $F8 then   // 5 个 1，1 个 0
-    Result := 5
-  else if B >= $F0 then   // 4 个 1，1 个 0
-    Result := 4
-  else if B >= $E0 then   // 3 个 1，1 个 0
-    Result := 3
-  else if B >= $B0 then   // 2 个 1，1 个 0
-    Result := 2
-  else                    // 其他
-    Result := 1;
-end;
-
-function GetByteWidthFromUtf16(Utf16Str: PWideChar): Integer;
-var
-  P: PCn2CharRec;
-  B1, B2: Byte;
-begin
-  Result := 2;
-
-  P := PCn2CharRec(Utf16Str);
-  B1 := GetUtf16HighByte(P);
-
-  if (B1 >= CN_UTF16_4CHAR_PREFIX1_LOW) and (B1 < CN_UTF16_4CHAR_PREFIX1_HIGH) then
-  begin
-    // 如果两个单字节字符，其值分别在 $D800 到 $DBFF 之间
-    Inc(P);
-    B2 := GetUtf16HighByte(P);
-
-    // 那么紧跟在后面的两个单字节字符应该在 $DC00 到 $DFFF 之间，
-    if (B2 >= CN_UTF16_4CHAR_PREFIX2_LOW) and (B2 < CN_UTF16_4CHAR_PREFIX2_HIGH) then
-      Result := 4;
-
-    // 这四个字节组成一个四字节 Unicode 字符，但并非该值的编码值
   end;
 end;
 
@@ -1399,91 +1225,6 @@ begin
 end;
 
 {$ENDIF}
-
-function GetCodePointFromUtf16Char(Utf16Str: PWideChar): TCnCodePoint;
-var
-  R: Word;
-  C2: PCn2CharRec;
-begin
-  if GetByteWidthFromUtf16(Utf16Str) = 4 then // 四字节字符
-    Result := GetCodePointFromUtf164Char(PAnsiChar(Utf16Str))
-  else  // 普通双字节字符
-  begin
-    C2 := PCn2CharRec(Utf16Str);
-    R := Byte(C2^.P1) shl 8 + Byte(C2^.P2);       // 双字节字符，值本身就是编码值
-
-{$IFDEF UTF16_BE}
-    Result := TCnCodePoint(R);
-{$ELSE}
-    Result := TCnCodePoint(UInt16ToBigEndian(R)); // UTF16-LE 要交换值
-{$ENDIF}
-  end;
-end;
-
-function GetCodePointFromUtf164Char(PtrTo4Char: Pointer): TCnCodePoint;
-var
-  TH, TL: Word;
-  C2: PCn2CharRec;
-begin
-  C2 := PCn2CharRec(PtrTo4Char);
-
-  // 第一个字节，去掉高位的 110110；第二个字节留着，共 2 + 8 = 10 位
-  TH := (GetUtf16HighByte(C2) and CN_UTF16_4CHAR_HIGH_MASK) shl 8 + GetUtf16LowByte(C2);
-  Inc(C2);
-
-  // 第三个字节，去掉高位的 110111，第四个字节留着，共 2 + 8 = 10 位
-  TL := (GetUtf16HighByte(C2) and CN_UTF16_4CHAR_HIGH_MASK) shl 8 + GetUtf16LowByte(C2);
-
-  // 高 10 位拼低 10 位
-  Result := TH shl 10 + TL + CN_UTF16_EXT_BASE;
-  // 码点减去 $10000 后的值，前 10 位映射到 $D800 到 $DBFF 之间，后 10 位映射到 $DC00 到 $DFFF 之间
-end;
-
-function GetUtf16CharFromCodePoint(CP: TCnCodePoint; PtrToChars: Pointer): Integer;
-var
-  C2: PCn2CharRec;
-  L, H: Byte;
-  LW, HW: Word;
-begin
-  if CP >= CN_UTF16_EXT_BASE then
-  begin
-    if PtrToChars <> nil then
-    begin
-      CP := CP - CN_UTF16_EXT_BASE;
-      // 拆出高 10 位放前两字节，拆出低 10 位放后两字节
-
-      LW := CP and CN_UTF16_4CHAR_SPLIT_MASK;          // 低 10 位，放三、四字节
-      HW := (CP shr 10) and CN_UTF16_4CHAR_SPLIT_MASK; // 高 10 位，放一、二字节
-
-      L := HW and $FF;
-      H := (HW shr 8) and CN_UTF16_4CHAR_HIGH_MASK;
-      H := H or CN_UTF16_4CHAR_PREFIX1_LOW;              // 1101 1000
-      C2 := PCn2CharRec(PtrToChars);
-
-      SetUtf16LowByte(L, C2);
-      SetUtf16HighByte(H, C2);
-
-      L := LW and $FF;
-      H := (LW shr 8) and CN_UTF16_4CHAR_HIGH_MASK;
-      H := H or CN_UTF16_4CHAR_PREFIX1_HIGH;              // 1101 1100
-      Inc(C2);
-
-      SetUtf16LowByte(L, C2);
-      SetUtf16HighByte(H, C2);
-    end;
-    Result := 2;
-  end
-  else
-  begin
-    if PtrToChars <> nil then
-    begin
-      C2 := PCn2CharRec(PtrToChars);
-      SetUtf16LowByte(Byte(CP and $00FF), C2);
-      SetUtf16HighByte(Byte(CP shr 8), C2);
-    end;
-    Result := 1;
-  end;
-end;
 
 function GetCodePointFromGB18030Char(PtrToGB18030Chars: PCnGB18030StringPtr): TCnCodePoint;
 var
--- Source/Common/CnWideStrings.pas
+++ Source/Common/CnWideStrings.pas
@@ -24,12 +24,14 @@ unit CnWideStrings;
 * 软件名称：开发包基础库
 * 单元名称：WideStrings 单元
 * 单元作者：CnPack 开发组
-* 备    注：该单元实现了简化的 TCnWideStringList 类
+* 备    注：该单元实现了简化的 TCnWideStringList 类与部分 Unicode 字符处理函数，
 *           以及扩展的 UTF8 到 UTF16 的编解码函数，支持 UTF16 中的四字节字符与 UTF8-MB4
 * 开发平台：WinXP SP3 + Delphi 5.0
 * 兼容测试：
 * 本 地 化：该单元中的字符串均符合本地化处理方式
-* 修改记录：202.11.10 V1.1
+* 修改记录：2022.11.25 V1.2
+*               从 CnGB18030 中搬移过来部分 Unicode 处理函数
+*           2022.11.10 V1.1
 *               UTF8 编码解码支持 UTF8-MB4 与 UTF16 中的四字节字符
 *           2010.01.16 by ZhouJingyu
 *               初始化提交
@@ -40,10 +42,36 @@ interface
 
 {$I CnPack.inc}
 
+// {$DEFINE UTF16_BE}
+
+// Delphi 默认 UTF16-LE，如果要处理 UTF16-BE 字符串，需要定义 UTF16_BE
+
 uses
-  Windows, SysUtils, Classes, IniFiles;
+  Windows, SysUtils, Classes, IniFiles, CnNative;
+
+const
+  CN_INVALID_CODEPOINT = $FFFFFFFF;
+  CN_ALTERNATIVE_CHAR  = '?';
 
 type
+  TCnCodePoint = type Cardinal;
+  {* 字符码值，或者叫码点，不等于表达的编码方式}
+
+  TCn2CharRec = packed record
+  {* 双字节字符结构}
+    P1: AnsiChar;
+    P2: AnsiChar;
+  end;
+  PCn2CharRec = ^TCn2CharRec;
+
+  TCn4CharRec = packed record
+  {* 四字节字符结构}
+    P1: AnsiChar;
+    P2: AnsiChar;
+    P3: AnsiChar;
+    P4: AnsiChar;
+  end;
+  PCn4CharRec = ^TCn4CharRec;
 
 { TCnWideStringList }
 
@@ -112,10 +140,57 @@ function CnUtf8DecodeToWideString(const S: AnsiString): WideString;
 {* 对 AnsiString 的 Utf8 解码得到 WideString，不做 Ansi 转换避免丢字符
   支持四字节 UTF16 字符与 UTF8-MB4}
 
+function GetUtf16HighByte(Rec: PCn2CharRec): Byte; {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
+{* 得到一个 UTF 16 双字节字符的高位字节值}
+
+function GetUtf16LowByte(Rec: PCn2CharRec): Byte; {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
+{* 得到一个 UTF 16 双字节字符的低位字节值}
+
+procedure SetUtf16HighByte(B: Byte; Rec: PCn2CharRec); {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
+{* 设置一个 UTF 16 双字节字符的高位字节值}
+
+procedure SetUtf16LowByte(B: Byte; Rec: PCn2CharRec); {$IFDEF SUPPORT_INLINE} inline; {$ENDIF}
+{* 设置一个 UTF 16 双字节字符的低位字节值}
+
+function GetCharLengthFromUtf8(Utf8Str: PAnsiChar): Integer;
+{* 计算一 UTF8（可能是 UTF8-MB4）字符串的字符数}
+
+function GetCharLengthFromUtf16(Utf16Str: PWideChar): Integer;
+{* 计算一 UTF16（可能混合 Unicode 扩展平面里的四字节字符）字符串的字符数}
+
+function GetByteWidthFromUtf8(Utf8Str: PAnsiChar): Integer;
+{* 计算一 UTF8（可能是 UTF8-MB4）字符串的当前字符占多少字节}
+
+function GetByteWidthFromUtf16(Utf16Str: PWideChar): Integer;
+{* 计算一 UTF16（可能混合 Unicode 扩展平面里的四字节字符）字符串的当前字符占多少字节}
+
+function GetCodePointFromUtf16Char(Utf16Str: PWideChar): TCnCodePoint;
+{* 计算一个 Utf16 字符的编码值（也叫代码位置），注意 Utf16Str 可能指向一个双字节字符，也可能指向一个四字节字符}
+
+function GetCodePointFromUtf164Char(PtrTo4Char: Pointer): TCnCodePoint;
+{* 计算一个四字节 Utf16 字符的编码值（也叫代码位置）}
+
+function GetUtf16CharFromCodePoint(CP: TCnCodePoint; PtrToChars: Pointer): Integer;
+{* 计算一个 Unicode 编码值的二字节或四字节表示，如果 PtrToChars 指向的位置不为空，
+  则将结果放在 PtrToChars 所指的二字节或四字节区域
+  调用者在 CP 超过 $FFFF 时须保证 PtrToChars 所指的区域至少四字节，反之二字节即可
+  返回 1 或 2，分别表示处理的是二字节或四字节}
+
 implementation
 
 uses
   CnGB18030;
+
+const
+  CN_UTF16_4CHAR_PREFIX1_LOW  = $D8;
+  CN_UTF16_4CHAR_PREFIX1_HIGH = $DC;
+  CN_UTF16_4CHAR_PREFIX2_LOW  = $DC;
+  CN_UTF16_4CHAR_PREFIX2_HIGH = $E0;
+
+  CN_UTF16_4CHAR_HIGH_MASK    = $3;
+  CN_UTF16_4CHAR_SPLIT_MASK   = $3FF;
+
+  CN_UTF16_EXT_BASE           = $10000;
 
 { TCnWideStringList }
 
@@ -752,6 +827,196 @@ begin
     SetLength(Result, L - 1)
   else
     Result := '';
+end;
+
+function GetUtf16HighByte(Rec: PCn2CharRec): Byte;
+begin
+{$IFDEF UTF16_BE}
+  Result := Byte(Rec^.P1);
+{$ELSE}
+  Result := Byte(Rec^.P2); // UTF16-LE 的高低位会置换
+{$ENDIF}
+end;
+
+function GetUtf16LowByte(Rec: PCn2CharRec): Byte;
+begin
+{$IFDEF UTF16_BE}
+  Result := Byte(Rec^.P2);
+{$ELSE}
+  Result := Byte(Rec^.P1); // UTF16-LE 的高低位会置换
+{$ENDIF}
+end;
+
+procedure SetUtf16HighByte(B: Byte; Rec: PCn2CharRec);
+begin
+{$IFDEF UTF16_BE}
+  Rec^.P1 := AnsiChar(B);
+{$ELSE}
+  Rec^.P2 := AnsiChar(B); // UTF16-LE 的高低位会置换
+{$ENDIF}
+end;
+
+procedure SetUtf16LowByte(B: Byte; Rec: PCn2CharRec);
+begin
+{$IFDEF UTF16_BE}
+  Rec^.P2 := AnsiChar(B);
+{$ELSE}
+  Rec^.P1 := AnsiChar(B); // UTF16-LE 的高低位会置换
+{$ENDIF}
+end;
+
+function GetCharLengthFromUtf8(Utf8Str: PAnsiChar): Integer;
+var
+  L: Integer;
+begin
+  Result := 0;
+  while Utf8Str^ <> #0 do
+  begin
+    L := GetByteWidthFromUtf8(Utf8Str);
+    Inc(Utf8Str, L);
+    Inc(Result);
+  end;
+end;
+
+function GetCharLengthFromUtf16(Utf16Str: PWideChar): Integer;
+var
+  L: Integer;
+begin
+  Result := 0;
+  while Utf16Str^ <> #0 do
+  begin
+    L := GetByteWidthFromUtf16(Utf16Str);
+    Utf16Str := PWideChar(TCnNativeInt(Utf16Str) + L);
+    Inc(Result);
+  end;
+end;
+
+function GetByteWidthFromUtf8(Utf8Str: PAnsiChar): Integer;
+var
+  B: Byte;
+begin
+  B := Byte(Utf8Str^);
+  if B >= $FC then        // 6 个 1，1 个 0，先不考虑七或八 1 的情况
+    Result := 6
+  else if B >= $F8 then   // 5 个 1，1 个 0
+    Result := 5
+  else if B >= $F0 then   // 4 个 1，1 个 0
+    Result := 4
+  else if B >= $E0 then   // 3 个 1，1 个 0
+    Result := 3
+  else if B >= $B0 then   // 2 个 1，1 个 0
+    Result := 2
+  else                    // 其他
+    Result := 1;
+end;
+
+function GetByteWidthFromUtf16(Utf16Str: PWideChar): Integer;
+var
+  P: PCn2CharRec;
+  B1, B2: Byte;
+begin
+  Result := 2;
+
+  P := PCn2CharRec(Utf16Str);
+  B1 := GetUtf16HighByte(P);
+
+  if (B1 >= CN_UTF16_4CHAR_PREFIX1_LOW) and (B1 < CN_UTF16_4CHAR_PREFIX1_HIGH) then
+  begin
+    // 如果两个单字节字符，其值分别在 $D800 到 $DBFF 之间
+    Inc(P);
+    B2 := GetUtf16HighByte(P);
+
+    // 那么紧跟在后面的两个单字节字符应该在 $DC00 到 $DFFF 之间，
+    if (B2 >= CN_UTF16_4CHAR_PREFIX2_LOW) and (B2 < CN_UTF16_4CHAR_PREFIX2_HIGH) then
+      Result := 4;
+
+    // 这四个字节组成一个四字节 Unicode 字符，但并非该值的编码值
+  end;
+end;
+
+function GetCodePointFromUtf16Char(Utf16Str: PWideChar): TCnCodePoint;
+var
+  R: Word;
+  C2: PCn2CharRec;
+begin
+  if GetByteWidthFromUtf16(Utf16Str) = 4 then // 四字节字符
+    Result := GetCodePointFromUtf164Char(PAnsiChar(Utf16Str))
+  else  // 普通双字节字符
+  begin
+    C2 := PCn2CharRec(Utf16Str);
+    R := Byte(C2^.P1) shl 8 + Byte(C2^.P2);       // 双字节字符，值本身就是编码值
+
+{$IFDEF UTF16_BE}
+    Result := TCnCodePoint(R);
+{$ELSE}
+    Result := TCnCodePoint(UInt16ToBigEndian(R)); // UTF16-LE 要交换值
+{$ENDIF}
+  end;
+end;
+
+function GetCodePointFromUtf164Char(PtrTo4Char: Pointer): TCnCodePoint;
+var
+  TH, TL: Word;
+  C2: PCn2CharRec;
+begin
+  C2 := PCn2CharRec(PtrTo4Char);
+
+  // 第一个字节，去掉高位的 110110；第二个字节留着，共 2 + 8 = 10 位
+  TH := (GetUtf16HighByte(C2) and CN_UTF16_4CHAR_HIGH_MASK) shl 8 + GetUtf16LowByte(C2);
+  Inc(C2);
+
+  // 第三个字节，去掉高位的 110111，第四个字节留着，共 2 + 8 = 10 位
+  TL := (GetUtf16HighByte(C2) and CN_UTF16_4CHAR_HIGH_MASK) shl 8 + GetUtf16LowByte(C2);
+
+  // 高 10 位拼低 10 位
+  Result := TH shl 10 + TL + CN_UTF16_EXT_BASE;
+  // 码点减去 $10000 后的值，前 10 位映射到 $D800 到 $DBFF 之间，后 10 位映射到 $DC00 到 $DFFF 之间
+end;
+
+function GetUtf16CharFromCodePoint(CP: TCnCodePoint; PtrToChars: Pointer): Integer;
+var
+  C2: PCn2CharRec;
+  L, H: Byte;
+  LW, HW: Word;
+begin
+  if CP >= CN_UTF16_EXT_BASE then
+  begin
+    if PtrToChars <> nil then
+    begin
+      CP := CP - CN_UTF16_EXT_BASE;
+      // 拆出高 10 位放前两字节，拆出低 10 位放后两字节
+
+      LW := CP and CN_UTF16_4CHAR_SPLIT_MASK;          // 低 10 位，放三、四字节
+      HW := (CP shr 10) and CN_UTF16_4CHAR_SPLIT_MASK; // 高 10 位，放一、二字节
+
+      L := HW and $FF;
+      H := (HW shr 8) and CN_UTF16_4CHAR_HIGH_MASK;
+      H := H or CN_UTF16_4CHAR_PREFIX1_LOW;              // 1101 1000
+      C2 := PCn2CharRec(PtrToChars);
+
+      SetUtf16LowByte(L, C2);
+      SetUtf16HighByte(H, C2);
+
+      L := LW and $FF;
+      H := (LW shr 8) and CN_UTF16_4CHAR_HIGH_MASK;
+      H := H or CN_UTF16_4CHAR_PREFIX1_HIGH;              // 1101 1100
+      Inc(C2);
+
+      SetUtf16LowByte(L, C2);
+      SetUtf16HighByte(H, C2);
+    end;
+    Result := 2;
+  end
+  else
+  begin
+    if PtrToChars <> nil then
+    begin
+      C2 := PCn2CharRec(PtrToChars);
+      SetUtf16LowByte(Byte(CP and $00FF), C2);
+      SetUtf16HighByte(Byte(CP shr 8), C2);
+    end;
+    Result := 1;
+  end;
 end;
 
 end.
