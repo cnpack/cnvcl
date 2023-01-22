4b30df22bc44e6538e8e9a8d1f6555ddbea7ccd6
Liu Xiao <liuxiao@cnpack.org>
2023-01-22 21:53:58 +0800

    + [Paillier] Add FMX Demo for Paillier.

---
+++ Examples/FMX/Paillier/TesetPaillier.dpr
@@ -0,0 +1,14 @@
+program TesetPaillier;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  UnitPaillier in 'UnitPaillier.pas' {FormPaillier};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormPaillier, FormPaillier);
+  Application.Run;
+end.
