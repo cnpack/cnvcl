95ea14502891060c5769bedb53c16c4a130f0069
Liu Xiao <liuxiao@cnpack.org>
2024-05-12 18:31:33 +0800

    * [Containers] Fix a Compiler Problem Under FPC.

--- Source/Common/CnContainers.pas
+++ Source/Common/CnContainers.pas
@@ -77,7 +77,8 @@ interface
 
 uses
   SysUtils, Classes, Contnrs, SyncObjs, CnNative
-  {$IFDEF COMPILER6_UP}, RTLConsts {$ELSE}, Consts {$ENDIF}
+  {$IFDEF FPC} , RTLConsts {$ELSE}
+  {$IFDEF COMPILER6_UP}, RTLConsts {$ELSE}, Consts {$ENDIF} {$ENDIF}
   {$IFDEF POSIX}, System.Generics.Collections {$ENDIF};
 
 {$DEFINE MULTI_THREAD} // 数学对象池支持多线程，性能略有下降，如不需要，注释此行即可
