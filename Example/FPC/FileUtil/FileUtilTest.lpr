0754110ed5877daa85f97b70033aa048db56f40e
Liu Xiao <liuxiao@cnpack.org>
2025-08-10 21:31:15 +0800

    + [Example] Add a FPC Demo for File Utils.

---
+++ Example/FPC/FileUtil/FileUtilTest.lpr
@@ -0,0 +1,28 @@
+program FileUtilTest;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, UnitFile
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  {$PUSH}{$WARN 5044 OFF}
+  Application.MainFormOnTaskbar:=True;
+  {$POP}
+  Application.Initialize;
+  Application.CreateForm(TFormFile, FormFile);
+  Application.Run;
+end.
+
---
+++ Example/FPC/FileUtil/UnitFile.pas
@@ -0,0 +1,68 @@
+unit UnitFile;
+{$mode objfpc}{$H+}
+interface
+
+uses
+  SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, Interfaces, CnFileUtils;
+
+type
+  TFormFile = class(TForm)
+    lblFind: TLabel;
+    edtPattern: TEdit;
+    lblIn: TLabel;
+    edtPath: TEdit;
+    btnFind: TButton;
+    mmoResult: TMemo;
+    btnBrowse: TButton;
+    dlgOpen1: TOpenDialog;
+    chkCancel: TCheckBox;
+    procedure btnBrowseClick(Sender: TObject);
+    procedure btnFindClick(Sender: TObject);
+  private
+    procedure OnFile(const FileName: string; const Info: TSearchRec;
+      var FindAbort: Boolean);
+    procedure OnDir(const SubDir: string);
+  public
+
+  end;
+
+var
+  FormFile: TFormFile;
+
+implementation
+
+{$R *.lfm}
+
+procedure TFormFile.OnDir(const SubDir: string);
+begin
+  mmoResult.Lines.Add('! ' + SubDir);
+end;
+
+procedure TFormFile.OnFile(const FileName: string;
+  const Info: TSearchRec; var FindAbort: Boolean);
+begin
+  Application.ProcessMessages;
+  if chkCancel.Checked then
+    FindAbort := True;
+  mmoResult.Lines.Add(FileName);
+end;
+
+procedure TFormFile.btnBrowseClick(Sender: TObject);
+begin
+  if dlgOpen1.Execute then
+    edtPath.Text := ExtractFilePath(dlgOpen1.FileName);
+end;
+
+procedure TFormFile.btnFindClick(Sender: TObject);
+begin
+  mmoResult.Lines.Clear;
+  chkCancel.Checked := False;
+
+  if CnFindFile(edtPath.Text, edtPattern.Text, @OnFile, @OnDir) then
+    ShowMessage('Find OK')
+  else
+    ShowMessage('Find Fail or Abort');
+end;
+
+end.
