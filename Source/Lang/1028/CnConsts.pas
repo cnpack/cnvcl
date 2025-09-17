1412b58524746e0832a5ddbb2c3b04446cbb131e
Liu Xiao <master@cnpack.org>
2025-09-17 21:33:55 +0800

    * Update Version Numbers.

--- Source/Lang/1028/CnConsts.pas
+++ Source/Lang/1028/CnConsts.pas
@@ -86,7 +86,7 @@ var
 const
   // 開發包信息
   SCnPackAbout = 'CnPack';
-  SCnPackVer = 'Ver 0.1.6.1';
+  SCnPackVer = 'Ver 0.1.7.0';
   SCnPackStr = SCnPackAbout + ' ' + SCnPackVer;
   SCnPackUrl = 'https://www.cnpack.org';
   SCnPackBbsUrl = 'https://bbs.cnpack.org';
