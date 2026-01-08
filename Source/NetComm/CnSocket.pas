d3423132481c00c7bfd1900727321ca44070eeab
Liu Xiao <liuxiao@cnpack.org>
2026-01-08 10:07:38 +0800

    * [Network] CnSocket Supports FPC under MacOS.

--- Source/NetComm/CnSocket.pas
+++ Source/NetComm/CnSocket.pas
@@ -24,7 +24,7 @@ unit CnSocket;
 * 软件名称：网络通讯组件包
 * 单元名称：网络通讯 Socket 公共声明与跨平台函数封装单元
 * 单元作者：CnPack 开发组
-* 备    注：
+* 备    注：需要支持 Delphi 及 FPC 编译器以及 Windows、Mac、Linux 平台
 * 开发平台：PWin7 + Delphi 5.0
 * 兼容测试：PWin9X/2000/XP/7 + Delphi 5/6
 * 本 地 化：该单元中的字符串均符合本地化处理方式
@@ -38,20 +38,28 @@ interface
 {$I CnPack.inc}
 
 uses
-  SysUtils, Classes {$IFDEF MSWINDOWS}, Windows, WinSock {$ELSE}, System.Net.Socket,
+  SysUtils, Classes {$IFDEF MSWINDOWS}, Windows, WinSock {$ELSE}
+  {$IFDEF FPC}, Sockets, BaseUnix {$ELSE}, System.Net.Socket,
   Posix.Base, Posix.NetIf, Posix.SysSocket, Posix.ArpaInet, Posix.NetinetIn,
-  Posix.Unistd, Posix.SysSelect, Posix.SysTime, Posix.Errno {$ENDIF};
+  Posix.Unistd, Posix.SysSelect, Posix.SysTime, Posix.Errno {$ENDIF}{$ENDIF};
 
 type
 {$IFDEF MSWINDOWS}
   TCnFDSet = TFDSet;
   PCnFDSet = PFDSet;
 {$ELSE}
+{$IFDEF FPC}
+  TCnFDSet = TFDSet;
+  PCnFDSet = PFDSet;
+  TSocket = LongInt;
+  TSockAddr = TInetSockAddr;
+{$ELSE}
   TCnFDSet = fd_set;
   PCNFDSet = Pfd_set;
 
   TSocket = Integer;
   TSockAddr = sockaddr_in;
+{$ENDIF}
 {$ENDIF}
 
 const
@@ -61,9 +69,13 @@ const
   INVALID_SOCKET = -1;
   SO_DONTLINGER  = $FF7F;
 
+{$IFNDEF FPC}
+
 function getifaddrs(var Ifap: pifaddrs): Integer; cdecl; external libc name _PU + 'getifaddrs';
 
 procedure freeifaddrs(Ifap: pifaddrs); cdecl; external libc name _PU + 'freeifaddrs';
+
+{$ENDIF}
 
 {$ENDIF}
 
@@ -133,6 +145,12 @@ function CnGetNetErrorNo: Integer;
 
 implementation
 
+{$IFNDEF MSWINDOWS}
+{$IFDEF FPC}
+function c_gethostname(name: PAnsiChar; len: LongInt): LongInt; cdecl; external 'libc' name 'gethostname';
+{$ENDIF}
+{$ENDIF}
+
 function CnGetHostName: string;
 var
   S: array[0..256] of AnsiChar;
@@ -140,7 +158,12 @@ begin
 {$IFDEF MSWINDOWS}
   WinSock.gethostname(@S[0], SizeOf(S));
 {$ELSE}
+{$IFDEF FPC}
+  FillChar(S[0], SizeOf(S), 0);
+  c_gethostname(@S[0], SizeOf(S));
+{$ELSE}
   Posix.Unistd.gethostname(@S[0], SizeOf(S));
+{$ENDIF}
 {$ENDIF}
   Result := string(S);
 end;
@@ -150,7 +173,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.socket(Af, Struct, Protocol);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpSocket(Af, Struct, Protocol);
+{$ELSE}
   Result := Posix.SysSocket.socket(Af, Struct, Protocol);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -159,7 +186,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.connect(S, Name, NameLen);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpConnect(S, @Name, SizeOf(Name));
+{$ELSE}
   Result := Posix.SysSocket.connect(S, sockaddr(Name), NameLen);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -168,16 +199,32 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.bind(S, Addr, NameLen);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpBind(S, @Addr, SizeOf(Addr));
+{$ELSE}
   Result := Posix.SysSocket.bind(S, sockaddr(Addr), NameLen);
+{$ENDIF}
 {$ENDIF}
 end;
 
 function CnGetSockName(S: TSocket; var Name: TSockAddr; var NameLen: Integer): Integer;
+{$IFNDEF MSWINDOWS}
+{$IFDEF FPC}
+var
+  SockLen: TSockLen;
+{$ENDIF}
+{$ENDIF}
 begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.getsockname(S, Name, NameLen);
 {$ELSE}
+{$IFDEF FPC}
+  SockLen := NameLen;
+  Result := fpGetSockName(S, @Name, @SockLen);
+  NameLen := SockLen;
+{$ELSE}
   Result := Posix.SysSocket.getsockname(S, sockaddr(Name), Cardinal(NameLen));
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -186,16 +233,32 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.listen(S, Backlog);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpListen(S, Backlog);
+{$ELSE}
   Result := Posix.SysSocket.listen(S, Backlog);
+{$ENDIF}
 {$ENDIF}
 end;
 
 function CnAccept(S: TSocket; Addr: PSockAddr; AddrLen: PInteger): TSocket;
+{$IFNDEF MSWINDOWS}
+{$IFDEF FPC}
+var
+  SockLen: TSockLen;
+{$ENDIF}
+{$ENDIF}
 begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.accept(S, Addr, AddrLen);
 {$ELSE}
+{$IFDEF FPC}
+  SockLen := AddrLen^;
+  Result := fpAccept(S, Addr, @SockLen);
+  AddrLen^ := SockLen;
+{$ELSE}
   Result := Posix.SysSocket.accept(S, Addr^, Cardinal(AddrLen^));
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -204,7 +267,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.send(S, Buf, Len, Flags);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpSend(S, @Buf, Len, Flags);
+{$ELSE}
   Result := Posix.SysSocket.send(S, Buf, Len, Flags);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -213,7 +280,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.recv(S, Buf, Len, Flags);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpRecv(S, @Buf, Len, Flags);
+{$ELSE}
   Result := Posix.SysSocket.recv(S, Buf, Len, Flags);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -223,17 +294,33 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.sendto(S, Buf, Len, Flags, AddrTo, ToLen);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpSendTo(S, @Buf, Len, Flags, @AddrTo, ToLen);
+{$ELSE}
   Result := Posix.SysSocket.sendto(S, Buf, Len, Flags, sockaddr(AddrTo), ToLen);
+{$ENDIF}
 {$ENDIF}
 end;
 
 function CnRecvFrom(S: TSocket; var Buf; Len, Flags: Integer;
   var AddrFrom: TSockAddr; var FromLen: Integer): Integer;
+{$IFNDEF MSWINDOWS}
+{$IFDEF FPC}
+var
+  SockLen: TSockLen;
+{$ENDIF}
+{$ENDIF}
 begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.recvfrom(S, Buf, Len, Flags, AddrFrom, FromLen);
 {$ELSE}
+{$IFDEF FPC}
+  SockLen := FromLen;
+  Result := fpRecvFrom(S, @Buf, Len, Flags, @AddrFrom, @SockLen);
+  FromLen := SockLen;
+{$ELSE}
   Result := Posix.SysSocket.recvfrom(S, Buf, Len, Flags, sockaddr(AddrFrom), Cardinal(FromLen));
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -243,7 +330,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.select(Nfds, Readfds, Writefds, Exceptfds, Timeout);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpSelect(Nfds, Readfds, Writefds, Exceptfds, Timeout);
+{$ELSE}
   Result := Posix.SysSelect.select(Nfds, Readfds, Writefds, Exceptfds, Timeout);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -253,7 +344,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.setsockopt(S, Level, OptName, OptVal, OptLen);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpSetSockOpt(S, Level, OptName, OptVal, OptLen);
+{$ELSE}
   Result := Posix.SysSocket.setsockopt(S, Level, OptName, OptVal, OptLen);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -262,7 +357,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.shutdown(S, How);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpShutdown(S, How);
+{$ELSE}
   Result := Posix.SysSocket.shutdown(S, How);
+{$ENDIF}
 {$ENDIF}
 end;
 
@@ -271,13 +370,21 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WinSock.closesocket(S);
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpClose(S);
+{$ELSE}
   Result := Posix.Unistd.__close(S);
+{$ENDIF}
 {$ENDIF}
 end;
 
 procedure CnFDZero(var FD: TCnFDSet);
 begin
+{$IFDEF FPC}
+  fpFD_ZERO(FD);
+{$ELSE}
   FD_ZERO(FD);
+{$ENDIF}
 end;
 
 procedure CnFDSet(F: Integer; var FD: TCnFDSet);
@@ -285,18 +392,30 @@ begin
 {$IFDEF MSWINDOWS}
   FD_SET(F, FD);
 {$ELSE}
+{$IFDEF FPC}
+  fpFD_SET(F, FD);
+{$ELSE}
   _FD_SET(F, FD);
+{$ENDIF}
 {$ENDIF}
 end;
 
 procedure CnFDClear(F: Integer; var FD: TCnFDSet);
 begin
+{$IFDEF FPC}
+  fpFD_CLR(F, FD);
+{$ELSE}
   FD_CLR(F, FD);
+{$ENDIF}
 end;
 
 function CnFDIsSet(F: Integer; var FD: TCnFDSet): Boolean;
 begin
+{$IFDEF FPC}
+  Result := fpFD_ISSET(F, FD) <> 0;
+{$ELSE}
   Result := FD_ISSET(F, FD);
+{$ENDIF}
 end;
 
 function CnGetNetErrorNo: Integer;
@@ -304,7 +423,11 @@ begin
 {$IFDEF MSWINDOWS}
   Result := WSAGetLastError;
 {$ELSE}
+{$IFDEF FPC}
+  Result := fpgeterrno;
+{$ELSE}
   Result := Posix.Errno.errno;
+{$ENDIF}
 {$ENDIF}
 end;
 
